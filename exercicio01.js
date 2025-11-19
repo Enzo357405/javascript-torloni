ee7658dd156bae10cb5054fad7fe7a387c1e7d87
ENZO HENRIQUE RODRIGUES MARQUES <51538706857@imaccommand023.local>
2025-11-19 14:50:01 -0300

    exerciocio 3 finalizado

--- exercicio01.js
+++ exercicio01.js
@@ -1,30 +1,42 @@
-let usuario = prompt("nome de usuário: ")
+// let usuario = prompt("nome de usuário: ")
 
-switch (usuario) {
-    case "admin":
-        console.log("usuário correto!")
+// switch (usuario) {
+//     case "admin":
+//         console.log("usuário correto!")
 
-        break;
+//         break;
 
 
 
-    default:
-        console.log("usuário incorreto.")
+//     default:
+//         console.log("usuário incorreto.")
 
-        break;
+//         break;
+// }
+
+// let senha = prompt("insira a senha")
+
+// switch (senha) {
+//     case "senha123":
+//         console.log("senha correta!")
+//         break;
+
+//     default:
+//         console.log("senha incorreta.")
+//         break;
+// }
+
+// window.alert("Login bem sucedido!")
+// console.log("Apresenta mensagem na tela")
+
+let username = prompt("digite o seu usuario: ")
+
+let senha = prompt("digite sua senha: ")
+
+if (username == "admin" && senha == "senha123") {
+    console.log("login bem sucedido")
 }
 
-let senha = prompt("insira a senha")
-
-switch (senha) {
-    case "senha123":
-        console.log("senha correta!")
-        break;
-
-    default:
-        console.log("senha incorreta.")
-        break;
+else {
+    console.log("login incorreto!;-;")
 }
-
-window.alert("Login feito com sucesso!")
-console.log("Apresenta mensagem na tela")
---
+++ exercicio03.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um numnero para saber c e impar ou par:"));
+
+if (numero % 2 === 0) {
+    alert("O número " + numero + " é Par.");
+} else {
+    alert("O número " + numero + " é Ímpar.");
+}
