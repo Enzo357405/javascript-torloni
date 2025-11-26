d3d0148fbc73d7a6f33fc58b32a9c3e3901c7592
ENZO HENRIQUE RODRIGUES MARQUES <51538706857@imaccommand023.local>
2025-11-26 14:37:15 -0300

    exercicio 08 finalizado

--- exercicio05.js
+++ exercicio05.js
@@ -1,11 +1,15 @@
 alert("escreva 3 numeros aleatorios")
 
-let n1 = parseInt (prompt("digite o primiro numero"))
+let n1 = parseInt (prompt("digite o primeiro numero"))
 let n2 = parseInt (prompt("digite o segundo numero"))
 let n3 = parseInt (prompt("digite o terceiro numero"))
 
 if(n1 < n2 && n2 < n3){
-    console.log("esta em ordem crescente");
-  
+    alert("esta em ordem crescente");
 }
-else{console.log("não esta em ordem crescente")}
+if(n1 > n2 && n2 > n3){
+    alert("Esta em ordem decrescente")
+}
+else{
+    alert("Esta em ordem aleatoria")
+}
