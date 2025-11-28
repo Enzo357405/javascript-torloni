b678cd29d8d2357519c87bf09af718ddb04427fc
ENZO HENRIQUE RODRIGUES MARQUES <51538706857@imaccommand023.local>
2025-11-28 13:38:42 -0300

    exercicio de array finalizado

---
+++ array/array.js
@@ -0,0 +1,27 @@
+// let frutas = ["goiaba", "morango", "melancia", "uva", "pera"]; //array/lista vazia
+
+// console. log("a lista de frutas tem" + frutas.length + "itens.");
+
+// console. log("a primeira fruta e a: " + frutas[0]);
+// console. log("a quarta fruta e a :" + frutas[3])
+
+// for(let i = 0; i < frutas.length; i++){
+//     console.log(frutas[i]);
+// }
+
+//lista de tarefas, com 10 itens//
+
+
+
+
+ //lista de tarefas, com 10 itens//
+
+
+
+
+let tarefas = ["Lavar a louça", "Varrer o chão", "Lavar o banheiro", "Arrumar os guarda-roupas", "Passar pano no chão", "Retirar o lixo", "Limpar os vidros", "Limpar o fogão", "Regar as plantas", "Pendurar as roupas no varal"]
+console.log("As tarefas são" +   tarefas.length  +  "itens")
+for(let i = 0; i < tarefas.length; i++){
+console.log(tarefas[i]);
+
+}
